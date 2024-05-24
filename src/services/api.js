7878370e316c8c3f0af03d3f9fb8030a718de048
William-Am0rim <160090242+William-Am0rim@users.noreply.github.com>
2024-05-24 01:08:29 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-ds5k.onrender.com'
+  baseURL: 'https://foodexplorer-backend-v0dv.onrender.com'
 });
